78eaca3bcc144a29ab67b0e4c1caa3b00a8c783f
narendar <narendar@gmail.com>
2025-09-13 06:40:05 +0530

    added Exceptions

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 
-const img = document.querySelector('img');
+const img = document.createElement('img');
 const key="g7LVOonZave5nJQYHqyBwNvyXjHy2MuP";
 let s="dog";
+const p=document.createElement("p");
 function search(){
    fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${key}&s=${s}`)
    .then(function(response) {
       return response.json();
    })
    .then(function(response) {
-      img.src = response.data.images.original.url;
+      img.setAttribute("src",response.data.images.original.url);
+      if(document.body.contains(p)){
+         document.body.removeChild(p);
+      };
+      document.body.appendChild(img);
+   }).catch(message=>{
+      p.textContent=`Unable to find the gif. search with proper spelling`;
+      let a=document.body.childNodes;
+      if(document.body.contains(img)){
+         document.body.removeChild(img);
+      };
+      document.body.appendChild(p);
    });
 }
 
@@ -18,4 +30,3 @@ btn.addEventListener("click",()=>{
    s=input.value;
    search();
 })
-search();
